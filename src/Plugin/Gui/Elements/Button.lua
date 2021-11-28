8eb6ace181a2cfcdd12fd1985cee11003ac206ff
gaby3xD <gaby3xd@gmail.com>
2021-11-28 13:27:32 +0800

    More elements

--- src/Plugin/Gui/Elements/Button.lua
+++ src/Plugin/Gui/Elements/Button.lua
@@ -1,27 +1,41 @@
-return function(theme, title, icon, callback)
+local gui = script.Parent.Parent
+local Theme = require(gui.Theme)
+
+return function(title, icon, callback)
 	local f = Instance.new("Frame")
 	f.BackgroundTransparency = 1
-	f.Size = UDim2.new(.8, 0, 0, theme.TextSize)
-
-	local i = Instance.new("ImageLabel")
-	i.BackgroundTransparency = 1
-	i.Image = "rbxassetid://" .. icon
-	i.ImageColor3 = theme.Foreground
-	i.ImageTransparency = theme.ForegroundTransparency
-	i.Size = UDim2.fromOffset(theme.TextSize, theme.TextSize)
-	i.Parent = f
+	f.Size = UDim2.new(.8, 0, 0, Theme.TextSize)
 
 	local b = Instance.new("TextButton")
-	b.Position = UDim2.fromOffset(theme.TextSize+5, 0)
-	b.Size = UDim2.new(1, -theme.TextSize-5, 0, theme.TextSize)
-	b.Font = Enum.Font[theme.Font]
+	b.Position = UDim2.fromOffset(0, 0)
+	b.Size = UDim2.new(1, 0, 1, 0)
+	b.Font = Enum.Font[Theme.Font]
 	b.BackgroundTransparency = 1
 	b.Text = title
-	b.TextSize = theme.TextSize
-	b.TextColor3 = theme.Foreground
-	b.TextTransparency = theme.ForegroundTransparency
+	b.TextSize = Theme.TextSize
+	b.TextColor3 = Theme.Foreground
+	b.TextTransparency = Theme.ForegroundTransparency
 	b.TextXAlignment = Enum.TextXAlignment.Left
 	b.Parent = f
+
+	if icon then
+		local i = Instance.new("ImageLabel")
+		i.BackgroundTransparency = 1
+		i.Image = "rbxassetid://" .. icon
+		i.ImageColor3 = Theme.Foreground
+		i.ImageTransparency = Theme.ForegroundTransparency
+		i.Size = UDim2.fromOffset(Theme.TextSize, Theme.TextSize)
+		i.Parent = f
+		b.Position = UDim2.fromOffset(Theme.TextSize+5, 0)
+		b.Size = UDim2.new(1, -Theme.TextSize-5, 1, 0)
+	end
+
+	b.MouseEnter:Connect(function()
+		b.TextColor3 = Theme.ForegroundHover
+	end)
+	b.MouseLeave:Connect(function()
+		b.TextColor3 = Theme.Foreground
+	end)
 
 	b.MouseButton1Click:Connect(callback)
 
---
+++ src/Plugin/Gui/Elements/Toolbar.lua
@@ -0,0 +1,12 @@
+local Elements = script.Parent
+local Button = require(Elements.Button)
+
+--[[
+	buttons
+	{
+		type = "text" or "image"
+	}
+]]
+return function(buttons)
+
+end
