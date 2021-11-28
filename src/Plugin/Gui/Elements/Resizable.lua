90e74192d06bfe1aa9b9e815dabb918e4c7dbd69
gaby3xD <gaby3xd@gmail.com>
2021-11-28 14:58:48 +0800

    Added flags and supported any anchorpoint

--- src/Plugin/Gui/Elements/Resizable.lua
+++ src/Plugin/Gui/Elements/Resizable.lua
@@ -14,35 +14,7 @@ local function dragger(size, pos, anc)
 	return d
 end
 
-return function(frame, minsize)
-	local dragTop = dragger(
-		UDim2.new(1, 0, 0, 10),
-		UDim2.fromScale(.5, 0),
-		Vector2.new(.5, 1)
-	)
-	dragTop.Parent = frame
-
-	local dragBot = dragger(
-		UDim2.new(1, 0, 0, 10),
-		UDim2.fromScale(.5, 1),
-		Vector2.new(.5, 0)
-	)
-	dragBot.Parent = frame
-
-	local dragLeft = dragger(
-		UDim2.new(0, 10, 1, 0),
-		UDim2.fromScale(0, .5),
-		Vector2.new(1, .5)
-	)
-	dragLeft.Parent = frame
-	
-	local dragRight = dragger(
-		UDim2.new(0, 10, 1, 0),
-		UDim2.fromScale(1, .5),
-		Vector2.new(0, .5)
-	)
-	dragRight.Parent = frame
-
+return function(frame, minsize, flags)
 	local startSize = frame.Size
 	local startPos = frame.Position
 
@@ -56,18 +28,57 @@ return function(frame, minsize)
 		startPos = frame.Position
 	end
 
-	dragTop.MouseButton1Down:Connect(function(x, y)
-		setstart(x, y, "top")
-	end)
-	dragBot.MouseButton1Down:Connect(function(x, y)
-		setstart(x, y, "bot")
-	end)
-	dragLeft.MouseButton1Down:Connect(function(x, y)
-		setstart(x, y, "lef")
-	end)
-	dragRight.MouseButton1Down:Connect(function(x, y)
-		setstart(x, y, "rig")
-	end)
+	if table.find(flags, "top") then
+		local dragTop = dragger(
+			UDim2.new(1, 0, 0, 10),
+			UDim2.fromScale(.5, 0),
+			Vector2.new(.5, 1)
+		)
+		dragTop.Parent = frame
+
+		dragTop.MouseButton1Down:Connect(function(x, y)
+			setstart(x, y, "top")
+		end)
+	end
+
+	if table.find(flags, "bot") then
+		local dragBot = dragger(
+			UDim2.new(1, 0, 0, 10),
+			UDim2.fromScale(.5, 1),
+			Vector2.new(.5, 0)
+		)
+		dragBot.Parent = frame
+
+		dragBot.MouseButton1Down:Connect(function(x, y)
+			setstart(x, y, "bot")
+		end)
+	end
+
+	if table.find(flags, "lef") then
+		local dragLeft = dragger(
+			UDim2.new(0, 10, 1, 0),
+			UDim2.fromScale(0, .5),
+			Vector2.new(1, .5)
+		)
+		dragLeft.Parent = frame
+
+		dragLeft.MouseButton1Down:Connect(function(x, y)
+			setstart(x, y, "lef")
+		end)
+	end
+
+	if table.find(flags, "rig") then
+		local dragRight = dragger(
+			UDim2.new(0, 10, 1, 0),
+			UDim2.fromScale(1, .5),
+			Vector2.new(0, .5)
+		)
+		dragRight.Parent = frame
+
+		dragRight.MouseButton1Down:Connect(function(x, y)
+			setstart(x, y, "rig")
+		end)
+	end
 
 	UserInputService.InputEnded:Connect(function(input, gameProcessedEvent)
 		if input.UserInputType == Enum.UserInputType.MouseButton1 then
@@ -84,65 +95,64 @@ return function(frame, minsize)
 		if holdStart then
 			local pos = UserInputService:GetMouseLocation()
 			local dst = holdStart - pos
-
-			if direction == "top" then
-				local maxed = math.max(minsize.Y, startSize.Y.Offset + dst.Y)
-				frame.Size = UDim2.new(
-					startSize.X.Scale,
-					startSize.X.Offset,
-					startSize.Y.Scale,
-					maxed
-				)
-
-				frame.Position = UDim2.new(
-					startPos.X.Scale,
-					startPos.X.Offset,
-					startPos.Y.Scale,
-					startPos.Y.Offset - ((maxed - startSize.Y.Offset) / 2)
-				)
-			elseif direction == "bot" then
-				local maxed = math.max(minsize.Y, startSize.Y.Offset - dst.Y)
-				frame.Size = UDim2.new(
-					startSize.X.Scale,
-					startSize.X.Offset,
-					startSize.Y.Scale,
-					maxed
-				)
-				frame.Position = UDim2.new(
-					startPos.X.Scale,
-					startPos.X.Offset,
-					startPos.Y.Scale,
-					startPos.Y.Offset + ((maxed - startSize.Y.Offset) / 2)
-				)
-			elseif direction == "lef" then
-				local maxed = math.max(minsize.X, startSize.X.Offset + dst.X)
-				frame.Size = UDim2.new(
-					startSize.X.Scale,
-					maxed,
-					startSize.Y.Scale,
-					startSize.Y.Offset
-				)
-				
-				frame.Position = UDim2.new(
-					startPos.X.Scale,
-					startPos.X.Offset - ((maxed - startSize.X.Offset) / 2),
-					startPos.Y.Scale,
-					startPos.Y.Offset
-				)
-			elseif direction == "rig" then
-				local maxed = math.max(minsize.X, startSize.X.Offset - dst.X)
-				frame.Size = UDim2.new(
-					startSize.X.Scale,
-					maxed,
-					startSize.Y.Scale,
-					startSize.Y.Offset
-				)
-				frame.Position = UDim2.new(
-					startPos.X.Scale,
-					startPos.X.Offset + ((maxed - startSize.X.Offset) / 2),
-					startPos.Y.Scale,
-					startPos.Y.Offset
-				)
+			if flags == nil or table.find(flags, direction) then
+				if direction == "top" then
+					local maxed = math.max(minsize.Y, startSize.Y.Offset + dst.Y)
+					frame.Size = UDim2.new(
+						startSize.X.Scale,
+						startSize.X.Offset,
+						startSize.Y.Scale,
+						maxed
+					)
+					frame.Position = UDim2.new(
+						startPos.X.Scale,
+						startPos.X.Offset,
+						startPos.Y.Scale,
+						startPos.Y.Offset - ((maxed - startSize.Y.Offset) * frame.AnchorPoint.X)
+					)
+				elseif direction == "bot" then
+					local maxed = math.max(minsize.Y, startSize.Y.Offset - dst.Y)
+					frame.Size = UDim2.new(
+						startSize.X.Scale,
+						startSize.X.Offset,
+						startSize.Y.Scale,
+						maxed
+					)
+					frame.Position = UDim2.new(
+						startPos.X.Scale,
+						startPos.X.Offset,
+						startPos.Y.Scale,
+						startPos.Y.Offset + ((maxed - startSize.Y.Offset) * frame.AnchorPoint.X)
+					)
+				elseif direction == "lef" then
+					local maxed = math.max(minsize.X, startSize.X.Offset + dst.X)
+					frame.Size = UDim2.new(
+						startSize.X.Scale,
+						maxed,
+						startSize.Y.Scale,
+						startSize.Y.Offset
+					)
+					frame.Position = UDim2.new(
+						startPos.X.Scale,
+						startPos.X.Offset - ((maxed - startSize.X.Offset) * frame.AnchorPoint.X),
+						startPos.Y.Scale,
+						startPos.Y.Offset
+					)
+				elseif direction == "rig" then
+					local maxed = math.max(minsize.X, startSize.X.Offset - dst.X)
+					frame.Size = UDim2.new(
+						startSize.X.Scale,
+						maxed,
+						startSize.Y.Scale,
+						startSize.Y.Offset
+					)
+					frame.Position = UDim2.new(
+						startPos.X.Scale,
+						startPos.X.Offset + ((maxed - startSize.X.Offset) * frame.AnchorPoint.X),
+						startPos.Y.Scale,
+						startPos.Y.Offset
+					)
+				end
 			end
 		end
 	end)
